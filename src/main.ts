fbdc32e2acda81200ac42f6732b3cd981f5f7bb1
mhezzet <mhezzet@gmail.com>
2019-07-07 18:31:13 +0200

    allowing cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   app.setGlobalPrefix('api');
   await app.listen(config.get('PORT'));
 }
